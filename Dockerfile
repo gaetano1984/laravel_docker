40936a011c832f670870a7025c84f82ab9760ef8
gaetano <gaetano.campanella@caffeina.com>
2022-11-08 15:52:13 +0100

    config mysql container

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM php:7.3-fpm-alpine
 
-#RUN docker-php-ext-install pdo pdo_mysql
+RUN docker-php-ext-install mysqli pdo pdo_mysql
 
 RUN curl -sS https://getcomposer.org/installer | php
 RUN mv composer.phar /usr/local/bin/composer
--- entrypoint/entrypoint_laravel.sh
+++ entrypoint/entrypoint_laravel.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+cd /var/www/html/laravel && composer install
+cd /var/www/html/laravel && cp .env.example .env
 cd /var/www/html/laravel && php artisan key:generate
+cd /var/www/html/laravel && php artisan migrate:install
+cd /var/www/html/laravel && php artisan migrate
 
 php-fpm
